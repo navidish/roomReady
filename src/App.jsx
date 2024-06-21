c251b2758770602b36ad3443e744f2fd8457981d
s.navidi <shnavidi18@gmail.com>
2024-06-21 08:14:20 +0330

    create new component for showing location detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header/Header';
+import Header from './components/Header';
 import Locations from './components/Locations';
 import LocationLayout from './components/locationLayout';
+import LocationsProvider from './context/LocationsProvider';
+import LocationDetail from './components/LocationDetail';
 
 function App() {
   return (
-    <>
+    <LocationsProvider>
       <Header />
       <Routes>
         <Route path="/" element={<Locations />} />
         <Route path="/locations" element={<LocationLayout />}>
           <Route index element={<Locations />} />
-          <Route path=":id" element={<p>single Location</p>} />
+          <Route path=":id" element={<LocationDetail />} />
         </Route>
       </Routes>
-    </>
+    </LocationsProvider>
   );
 }
 
--- src/context/LocationsProvider.jsx
+++ src/context/LocationsProvider.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable no-unused-vars */
-import { createContext, useContext } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import useFetch from '../hooks/useFetch';
+import axios from 'axios';
 
 const Locationcontext = createContext();
 const BASE_URL = 'http://localhost:5000/hotels';
 
 function LocationsProvider({ children }) {
+  const [currentLocation, setCurrentLocation] = useState({});
+  const [isLoadingCurrLocation, setIsLoadinCurrLocation] = useState(false);
   const [searchParams, setSearchParams] = useSearchParams();
   const destination = searchParams.get('destination');
   const room = JSON.parse(searchParams.get('options'))?.find(
@@ -17,9 +20,27 @@ function LocationsProvider({ children }) {
     BASE_URL,
     `q=${destination || ''}&accommodates_gte=${room || 1}`
   );
+  async function getLocation(id) {
+    setIsLoadinCurrLocation(true);
+    try {
+      const { data } = await axios.get(`${BASE_URL}/${id}`);
+      setCurrentLocation(data);
+      setIsLoadinCurrLocation(false);
+    } catch (error) {
+      setIsLoadinCurrLocation(false);
+    }
+  }
 
   return (
-    <Locationcontext.Provider value={{ isLoading, locations }}>
+    <Locationcontext.Provider
+      value={{
+        isLoading,
+        locations,
+        getLocation,
+        currentLocation,
+        isLoadingCurrLocation,
+      }}
+    >
       {children}
     </Locationcontext.Provider>
   );

e9ae801ebb3db919d28d5dededf8c1cbe958bfb8
s.navidi <shnavidi18@gmail.com>
2024-06-21 07:25:44 +0330

    show locations on map

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,28 +1,51 @@
 /* eslint-disable no-unused-vars */
-import { useState } from 'react';
-import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { useEffect, useState } from 'react';
+import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import { useLocations } from '../../context/LocationsProvider';
+import { useSearchParams } from 'react-router-dom';
 const Map = () => {
-  const [position, setPosition] = useState([35.7219, 51.3347]);
+  const { isLoading, locations } = useLocations();
+  const [zoomLevel, setZoomLevel] = useState(4);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
+  const [position, setPosition] = useState([20, 5]);
+
+  useEffect(() => {
+    if (lat && lng) {
+      setZoomLevel(8);
+      setPosition([lat, lng]);
+    }
+    setPosition([locations[0]?.latitude, locations[0]?.longitude]);
+  }, [lat, lng]);
   return (
     <MapContainer
       className="h-full"
       center={position}
-      zoom={13}
+      zoom={zoomLevel}
       scrollWheelZoom={true}
     >
+      <ChangeCenter position={position} />
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
       />
-      <Marker position={position}>
-        <Popup>
-          <div className='flex justify-center items-center flex-col'>
-          <p className="text-primary-600">hi shasan</p>
-          <button className=' rounded border bg-primary-600  text-white font-bold p-1'>save</button>
-          </div>
-        </Popup>
-      </Marker>
+      {locations?.map((item) => (
+        <Marker key={item.id} position={[item?.latitude, item?.longitude]}>
+          <Popup>
+            <div className="flex justify-center items-center flex-col">
+              {item.market}
+              {item.street}
+            </div>
+          </Popup>
+        </Marker>
+      ))}
     </MapContainer>
   );
 };
 export default Map;
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}

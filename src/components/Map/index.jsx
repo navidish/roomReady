41b86605e855489af3ef90e6c0a59e52ad5b3a76
s.navidi <shnavidi18@gmail.com>
2024-06-26 02:41:26 +0330

    create custom marker that shows pricec on map and show hotel location on marker popup

--- src/components/Map/index.jsx
+++ src/components/Map/index.jsx
@@ -1,46 +1,77 @@
 /* eslint-disable no-unused-vars */
 import { useEffect, useState } from 'react';
 import { MapContainer, Marker, Popup, TileLayer, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import { useLocations } from '../../context/LocationsProvider';
 import { useSearchParams } from 'react-router-dom';
+import Slider from '../Slider';
+import { FaStar } from 'react-icons/fa6';
+
 const Map = () => {
   const { isLoading, locations } = useLocations();
-  const [zoomLevel, setZoomLevel] = useState(4);
+  const [zoomLevel, setZoomLevel] = useState(8);
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get('lat');
-  const lng = searchParams.get('lng');
-  const [position, setPosition] = useState([20, 5]);
+  const lat = searchParams.get('lat') ?? 20;
+  const lng = searchParams.get('lng') ?? 5;
+  const [position, setPosition] = useState([41, -73]);
+  const CustomMarker = ({ text }) => {
+    const customIcon = L.divIcon({
+      className: '',
+      iconSize: [24, 24],
+      html: `<div class="w-12 h-8 font-extrabold text-gray-800 bg-white rounded-full flex items-center justify-center shadow-md">${text}</div>`,
+    });
+    return customIcon;
+  };
 
-  useEffect(() => {
-    if (lat && lng) {
-      setZoomLevel(8);
-      setPosition([lat, lng]);
-    }
-    setPosition([locations[0]?.latitude, locations[0]?.longitude]);
-  }, [lat, lng]);
   return (
-    <MapContainer
-      className="h-full"
-      center={position}
-      zoom={zoomLevel}
-      scrollWheelZoom={true}
-    >
-      <ChangeCenter position={position} />
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-      />
-      {locations?.map((item) => (
-        <Marker key={item.id} position={[item?.latitude, item?.longitude]}>
-          <Popup>
-            <div className="flex justify-center items-center flex-col">
-              {item.market}
-              {item.street}
-            </div>
-          </Popup>
-        </Marker>
-      ))}
-    </MapContainer>
+    <div className="flex-1 relative bg-gray-100 z-0">
+      <MapContainer
+        className="h-screen"
+        center={position}
+        zoom={zoomLevel}
+        scrollWheelZoom={true}
+      >
+        <ChangeCenter position={position} />
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {locations?.map(({ id, listing }) => (
+          <Marker
+            key={id}
+            icon={CustomMarker({ text: listing.price })}
+            position={[
+              listing?.coordinate?.latitude,
+              listing?.coordinate?.longitude,
+            ]}
+          >
+            <Popup className="w-96">
+              <div key={id}>
+                <Slider images={listing.contextualPictures} />
+                <div className="flex justify-between items-center">
+                  <p className="font-extrabold">{listing.title}</p>
+                  <div className="flex justify-center items-center gap-1">
+                    <FaStar className="w-4 h-4" />
+                    <p className="text-gray-900">
+                      {listing.avgRatingLocalized ?? 0}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex gap-2">
+                  <p className="font-extrabold">
+                    {listing.price}
+                    <span className="font-normal ml-1">
+                      night<strong>.</strong>
+                    </span>
+                  </p>
+                  <p className="text-gray-700">{listing.secondaryLine}</p>
+                </div>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
   );
 };
 export default Map;

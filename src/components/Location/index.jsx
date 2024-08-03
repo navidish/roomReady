d9e0018e8be3eda4a4f8f47185001ea5b5e97e8c
s.navidi <shnavidi18@gmail.com>
2024-08-03 04:04:27 +0330

    add review section to db.json and locationDetail

--- src/components/Location/index.jsx
+++ src/components/Location/index.jsx
@@ -1,34 +1,40 @@
 /* eslint-disable no-unused-vars */
+import L from 'leaflet';
+import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useLocations } from '../../context/LocationsProvider';
-import { useEffect, useState } from 'react';
-import { IoShareOutline } from 'react-icons/io5';
-import { IoHeartOutline } from 'react-icons/io5';
+import { IoShareOutline, IoHeartOutline } from 'react-icons/io5';
 import LeftSign from '../../../assets/leftSign.png';
 import RightSign from '../../../assets/RightSign.png';
-import { FaStar } from 'react-icons/fa6';
-import { TiHome } from 'react-icons/ti';
-import { FaChevronLeft } from 'react-icons/fa6';
-import { FaChevronRight } from 'react-icons/fa6';
-import { MapContainer, Marker, TileLayer } from 'react-leaflet';
+import { FaChevronLeft, FaChevronRight, FaStar } from 'react-icons/fa6';
 import IconComponent from '../IconComponent';
 
+const svgIcon = new L.divIcon({
+  html: `<div class=" w-12 h-12 flex justify-center items-center rounded-full bg-primary-600  p-3"><svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false" style="display: block; height: 22px; width: 22px; fill: #fff;"><path d="m8.94959955 1.13115419 5.71719515 4.68049298c.2120231.18970472.3332053.46073893.3332053.74524138v7.94311145c0 .2761424-.2238576.5-.5.5h-4.5v-5.5c0-.24545989-.17687516-.44960837-.41012437-.49194433l-.08987563-.00805567h-3c-.27614237 0-.5.22385763-.5.5v5.5h-4.5c-.27614237 0-.5-.2238576-.5-.5v-7.95162536c0-.28450241.12118221-.55553661.3502077-.75978249l5.70008742-4.65820288c.55265671-.45163993 1.34701168-.45132001 1.89930443.00076492z"></path></svg></div>`,
+  iconSize: [24, 24],
+  className: '',
+});
 const Location = () => {
   const { id } = useParams();
   const { getLocation, isLoadingCurrLocation, currentLocation } =
     useLocations();
   const { listing, detail } = currentLocation;
-  const [sleep,setSleep]=useState(detail?.sleepData?.arrangementDetails);
+  const [sleep, setSleep] = useState(detail?.sleepData?.arrangementDetails);
+
   useEffect(() => {
-    setSleep(detail?.sleepData?.arrangementDetails?.slice(0,2))
     getLocation(id);
   }, [id]);
-  console.log('listing',listing)
+
+  useEffect(() => {
+    setSleep(detail?.sleepData?.arrangementDetails?.slice(0, 2));
+  }, [detail]);
 
   return (
     <>
+      <div className="w-full h-[1px] bg-gray-200 mt-4 mb-8"></div>
       {!!listing && (
-        <div className='mx-[200px]'>
+        <div className="mx-[86px]">
           <div>
             <div className="flex justify-between items-center mt-4">
               <h1 className="font-bold text-gray-800 text-4xl">
@@ -70,7 +76,7 @@ const Location = () => {
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-3 mt-4 ">
+          <div className="grid grid-cols-3 mt-4 gap-3 ">
             <div className="col-span-2 pr-5">
               <h2 className="font-semibold text-[22px] text-gray-800">
                 Entire Home in {listing.title},united state
@@ -80,26 +86,36 @@ const Location = () => {
               </p>
 
               <div className="flex justify-between border p-4 mt-4 w-5/6 gap-2 border-gray-300 rounded-2xl">
-              <div className='flex'>
-                  <img  className='p-2' src={LeftSign} />
-                    <div className="font-semibold text-center">
-                        <p>Guest</p>
-                        <p>favorite</p>
-                    </div>
-                  <img  className='p-2' src={RightSign} />
-              </div>
-                <div className='w-60'>
-                  <p className='font-semibold text-base'>{listing?.brand?.description}</p>
+                <div className="flex">
+                  <img className="p-2" src={LeftSign} />
+                  <div className="font-semibold text-center">
+                    <p>Guest</p>
+                    <p>favorite</p>
+                  </div>
+                  <img className="p-2" src={RightSign} />
+                </div>
+                <div className="w-60">
+                  <p className="font-semibold text-base">
+                    {listing?.brand?.description}
+                  </p>
                 </div>
                 <div className="flex flex-col justify-center items-center gap-1">
-                  <p className='font-bold text-2xl'>{listing.avgRatingLocalized}</p>
+                  <p className="font-bold text-2xl">
+                    {listing.avgRatingLocalized}
+                  </p>
                   <div className="flex space-x-[2px]">
-                    {[...Array(5).keys().map((_e)=><FaStar className="w-3 h-3" />)]}
+                    {[
+                      ...Array(5)
+                        .keys()
+                        .map((_e) => (
+                          <FaStar key={_e.index} className="w-3 h-3" />
+                        )),
+                    ]}
                   </div>
                 </div>
                 <div className="w-[2px] h-8 my-auto mx-1 bg-gray-300 border-0 rounded-full "></div>
                 <div className="flex flex-col justify-center items-center">
-                  <p className='font-bold text-2xl'>25</p>
+                  <p className="font-bold text-2xl">25</p>
                   <p>Reviews</p>
                 </div>
               </div>
@@ -107,49 +123,64 @@ const Location = () => {
                 <HighLighContainer key={index} highlight={_high} />
               ))}
 
-            <div>
-              <div className='flex justify-between items-center'>
-              <h3 className="font-bold text-gray-800 text-4xl my-4">
-                Where you’ll sleep
-              </h3>
-              <div className='flex gap-4'>
-
-          <div
-            onClick={()=>setSleep(detail?.sleepData?.arrangementDetails?.slice(0,2))}
-            className={`${sleep?.some((_e)=>_e.id==1)} ? disabled:cursor-not-allowed : border w-10 h-10 flex items-center justify-center border-gray-100 shadow-md text-2xl rounded-full p-2 text-block bg-white cursor-pointer`}>
-            <FaChevronLeft size={12} />
-          </div>
-
-          <div
-            onClick={()=>setSleep(detail?.sleepData?.arrangementDetails?.slice(sleep?.length,sleep?.length+2))}
-            className="border w-10 h-10 flex items-center justify-center border-gray-100 shadow-md text-2xl rounded-full p-2 text-block bg-white cursor-pointer"
-          >
-            <FaChevronRight size={12} />
-          </div>
-       
+              <div className="mr-4">
+                <h3 className="font-bold text-gray-800 text-2xl my-4">
+                  What this place offers
+                </h3>
+                <div className="grid grid-cols-2">
+                  {detail?.houseAmenities?.amenities?.map((amenitie) => (
+                    <Amenities key={amenitie.id} amenitie={amenitie} />
+                  ))}
+                </div>
               </div>
-              </div>
-            
-              
-              <div className='grid grid-cols-2 gap-2 w-full'>
-              {
-                sleep?.map((_elm)=>(
-                  <div >
-                    <img className='rounded-3xl' src={_elm.src}/>
-                    <p>{_elm.title}</p>
-                    <p>{_elm.subtitle}</p>
+
+              <div>
+                <div className="flex justify-between items-center">
+                  <h3 className="font-bold text-gray-800 text-2xl my-4">
+                    Where you’ll sleep
+                  </h3>
+                  <div className="flex gap-4">
+                    <div
+                      onClick={() =>
+                        setSleep(
+                          detail?.sleepData?.arrangementDetails?.slice(0, 2)
+                        )
+                      }
+                      className={`${sleep?.some(
+                        (_e) => _e.id == 1
+                      )} ? disabled:cursor-not-allowed : border w-10 h-10 flex items-center justify-center border-gray-100 shadow-md text-2xl rounded-full p-2 text-block bg-white cursor-pointer`}
+                    >
+                      <FaChevronLeft size={12} />
+                    </div>
+
+                    <div
+                      onClick={() =>
+                        setSleep(
+                          detail?.sleepData?.arrangementDetails?.slice(
+                            sleep?.length,
+                            sleep?.length + 2
+                          )
+                        )
+                      }
+                      className="border w-10 h-10 flex items-center justify-center border-gray-100 shadow-md text-2xl rounded-full p-2 text-block bg-white cursor-pointer"
+                    >
+                      <FaChevronRight size={12} />
+                    </div>
                   </div>
-                ))
-              }
-               </div>
-            
-          
-          </div>
+                </div>
 
-             
-              
+                <div className="grid grid-cols-2 gap-2 w-full">
+                  {sleep?.map((_elm) => (
+                    <div key={_elm.src}>
+                      <img className="rounded-3xl" src={_elm.src} />
+                      <p className="font-semibold pt-2">{_elm.title}</p>
+                      <p>{_elm.subtitle}</p>
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
-            <div className=" shadow-2xl p-4 rounded-xl mt-8 sticky top-16 h-[300px]">
+            <div className="shadow-xl p-4 rounded-xl mt-8 sticky top-16 h-[300px]">
               <form onSubmit={() => console.log('clicked')}>
                 <div className="mb-4 relative">
                   <label className="block mb-1" htmlFor="email">
@@ -182,36 +213,55 @@ const Location = () => {
               </form>
             </div>
           </div>
-          <div className='flex flex-col justify-center items-center'>
-            <h3 className='text-2xl font-bold'>{detail?.ratingdata?.heading?.title}</h3>
-            <p className='line-clamp-2'>{detail?.ratingdata?.heading?.subtitle}</p>
-          </div>
-          <div className='grid grid-cols-7 divide-x my-8'>
-                <>
-                <div className='mr-4'>
-                <p className='text-gray-800 text-lg font-semibold'>Overall rating</p>
-                {
-                  detail?.ratingdata?.ratingDistribution?.map((_e)=>(
-                    <div className='flex items-center justify-center gap-2 '>
-                      <p className='text-sm'>{_e.label}</p>
-                      <div className="w-full bg-gray-200 rounded-full ">
-                      <div className={"bg-gray-700 h-1 rounded-full "} style={{width:_e.localizedRating}}></div>
-                      </div>
-                   </div>
-                  )
-                  )
-                }
-                </div>
-                  
-                {
-                detail?.ratingdata?.ratings?.map((_rat,index)=>(
-                  <RatingContainer key={index} reating={_rat}/>
-                ))}
-                </>
-              
+          <div className="w-full h-[1px] bg-gray-200 my-4"></div>
+          <div className="my-8">
+            <div className="text-center">
+              <h3 className="text-2xl font-bold">
+                {detail?.ratingdata?.heading?.title}
+              </h3>
+              <div className="w-full py-4 flex justify-center items-center">
+                <p className="w-[328px]">
+                  {detail?.ratingdata?.heading?.subtitle}
+                </p>
               </div>
+            </div>
+            <div className="grid grid-cols-7 divide-x my-8">
+              <>
+                <div className="mr-4">
+                  <p className="text-gray-800 text-lg font-semibold">
+                    Overall rating
+                  </p>
+                  {detail?.ratingdata?.ratingDistribution?.map((_e) => (
+                    <div
+                      key={_e.label}
+                      className="flex items-center justify-center gap-2 "
+                    >
+                      <p className="text-sm">{_e.label}</p>
+                      <div className="w-full bg-gray-200 rounded-full ">
+                        <div
+                          className={'bg-gray-700 h-1 rounded-full '}
+                          style={{ width: _e.localizedRating }}
+                        ></div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                {detail?.ratingdata?.ratings?.map((_rat, index) => (
+                  <RatingContainer key={index} reating={_rat} />
+                ))}
+              </>
+            </div>
+          </div>
+          <div className="w-full h-[1px] bg-gray-200 my-4"></div>
+          <div className="grid grid-cols-2  space-x-2 space-y-2">
+            {detail?.reviews?.map((review) => (
+              <Review key={review.id} review={review} />
+            ))}
+          </div>
+          <div className="w-full h-[1px] bg-gray-200 my-4"></div>
           <div>
-            <h3 className="font-bold text-gray-800 text-4xl my-4">
+            <h3 className="font-bold text-gray-800 text-2xl my-4">
               Where you’ll be
             </h3>
             <p className=" text-gray-600 text-xl my-4">
@@ -233,7 +283,7 @@ const Location = () => {
 
                 <Marker
                   key={id}
-                  //icon={TiHome}
+                  icon={svgIcon}
                   position={[
                     listing?.coordinate?.latitude,
                     listing?.coordinate?.longitude,
@@ -242,7 +292,7 @@ const Location = () => {
               </MapContainer>
             </div>
           </div>
-          <div>review</div>
+
           <div>guest info</div>
         </div>
       )}
@@ -252,11 +302,11 @@ const Location = () => {
 export default Location;
 
 const HighLighContainer = ({ highlight }) => {
-  const { title, subtitle , icon } = highlight;
- 
+  const { title, subtitle, icon } = highlight;
+
   return (
     <div className="flex items-center gap-4 p-2">
-      <IconComponent icon={icon}/>
+      <IconComponent icon={icon} />
       <div>
         <p className="text-gray-800 text-base font-semibold py-2">{title}</p>
         <p className="text-gray-700 text-sm">{subtitle}</p>
@@ -264,15 +314,68 @@ const HighLighContainer = ({ highlight }) => {
     </div>
   );
 };
+
 const RatingContainer = ({ reating }) => {
-  const { label, localizedRating ,icon} = reating;
+  const { label, localizedRating, icon } = reating;
   return (
-      <div className='pl-2 flex flex-col justify-between'>
-       <div>
+    <div className="pl-4 flex flex-col justify-between">
+      <div>
         <p className="text-gray-800 text-lg font-semibold">{label}</p>
         <p className="text-gray-800 text-lg  font-bold">{localizedRating}</p>
-       </div>
-       <div className='mt-8'> <IconComponent  icon={icon}/></div>
+      </div>
+      <div className="mt-8">
+        <IconComponent icon={icon} />
+      </div>
+    </div>
+  );
+};
+const Amenities = ({ amenitie }) => {
+  const { title, icon } = amenitie;
+  return (
+    <div className="flex items-center gap-4 p-2">
+      <IconComponent icon={icon} />
+      <div>
+        <p className="text-gray-800 text-base">{title}</p>
+      </div>
+    </div>
+  );
+};
+
+const Review = ({ review }) => {
+  const {
+    hostName,
+    pictureUrl,
+    comments,
+    createdAt,
+    localizedDate,
+    localizedReviewerLocation,
+    rating,
+    reviewHighlight,
+  } = review;
+
+  return (
+    <div className="my-4 ">
+      <div className="flex gap-2">
+        <img className="w-[56px] h-[56px] rounded-full" src={pictureUrl} />
+        <div>
+          <p className="font-semibold">{hostName}</p>
+          <p>{localizedReviewerLocation}</p>
+        </div>
+      </div>
+      <div className="flex items-center  py-2">
+        <div className="flex space-x-[2px]">
+          {[
+            ...Array(rating)
+              .keys()
+              .map((_e) => <FaStar key={_e.index} className="w-3 h-3" />),
+          ]}
+        </div>
+        &nbsp;&bull;&nbsp;
+        <span>{localizedDate}</span>
+        &nbsp;&bull;&nbsp;
+        <span className="text-gray-400"> {reviewHighlight}</span>
+      </div>
+      <p>{comments}</p>
     </div>
   );
 };

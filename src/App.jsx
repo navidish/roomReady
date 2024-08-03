109267c1ee17d743edc19924ec0757fa09c34471
s.navidi <shnavidi18@gmail.com>
2024-08-03 04:10:47 +0330

    delete unused class

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Locations from './components/Locations';
-import LocationLayout from './components/locationLayout';
 import LocationsProvider from './context/LocationsProvider';
-import LocationDetail from './components/LocationDetail';
 import Map from './components/Map';
 import Login from './components/Login';
 import AuthProvier from './context/AuthProvider';
@@ -19,10 +17,6 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route path="/" element={<Locations />} />
             <Route path="/location/:id" element={<Location />} />
-            <Route path="/locations" element={<LocationLayout />}>
-              <Route index element={<Locations />} />
-              <Route path=":id" element={<LocationDetail />} />
-            </Route>
             <Route path="/Maps" element={<Map />} />
           </Routes>
         </LocationsProvider>
